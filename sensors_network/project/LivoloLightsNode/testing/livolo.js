74b2867e0a361bb998ab97cb5e31d1a1afe20cf9
Marius Tiutiu <mtiutiu@softvision.ro>
2019-09-24 18:01:20 +0300

    ref

--- sensors_network/project/LivoloLightsNode/testing/livolo.js
+++ sensors_network/project/LivoloLightsNode/testing/livolo.js
@@ -1,16 +1,17 @@
 // ---------- GPIO PINS MAPPING -----------
-const S1_LED = 8;
-const S2_LED = 8;
+const S1_LED = 18;
+const S2_LED = 5;
 
-const MTSA = 28;
-const TS1 = 29;
-const MTPM = 30;
+const MTSA = 21;
+const TS1 = 1;
+const TS2 = 0;
+const MTPM = 15;
 
-const R1_SET = 5;
-const R1_RESET = 1;
+const R1_SET = 6;
+const R1_RESET = 8;
 
-const R2_SET = 0;
-const R2_RESET = 6;
+const R2_SET = 30;
+const R2_RESET = 29;
 
 const CHANNEL1 = 0;
 const CHANNEL2 = 1;
@@ -19,19 +20,19 @@ const OFF = 0;
 const ON = 1;
 
 const CHANNEL_PINS_MAP = {
-  CHANNEL1: {
-    ON: R1_SET,
-    OFF: R1_RESET
+  0: {
+    1: R1_SET,
+    0: R1_RESET
   },
-  CHANNEL2: {
-    ON: R2_SET,
-    OFF: R2_RESET
+  1: {
+    1: R2_SET,
+    0: R2_RESET
   }
 };
 
 const CHANNEL_LEDS_MAP = {
-  CHANNEL1: S1_LED,
-  CHANNEL2: S2_LED
+  0: S1_LED,
+  1: S2_LED
 };
 
 const BOOT_LIGHT_OFF_DELAY_MS = 1000;
@@ -48,10 +49,6 @@ const TX_POWER_LVL_DB = 4;
 const LIGHT_SERVICE_ID = 0xBCDE;
 const LIGHT1_CHARACTERISTIC_ID = 0xABCD;
 const LIGHT2_CHARACTERISTIC_ID = 0xABCE;
-const LIGHT_CHARACTERISTICS_MAP = {
-  CHANNEL1: LIGHT1_CHARACTERISTIC_ID,
-  CHANNEL2: LIGHT2_CHARACTERISTIC_ID
-};
 // --------------------------------------
 
 
@@ -59,19 +56,21 @@ function setLightState(channel, value) {
   digitalWrite(CHANNEL_PINS_MAP[channel][value], HIGH);
   digitalWrite(CHANNEL_LEDS_MAP[channel], !value);
   state[channel] = value;
-  var lightCharacteristic = LIGHT_CHARACTERISTICS_MAP[channel];
-  console.log(ON);
 
   setTimeout(function() {
     digitalWrite(CHANNEL_PINS_MAP[channel][value], LOW);
-    NRF.updateServices({
-      LIGHT_SERVICE_ID : {
-        lightCharacteristic : {
-          value : state[channel],
+    /*NRF.updateServices({
+      0xBCDE: {
+        0xABCD: {
+          value : state[CHANNEL1],
+          notify: true
+        },
+        0xABCE: {
+          value : state[CHANNEL2],
           notify: true
         }
       }
-    });
+    });*/
   }, RELAY_PULSE_WIDTH_MS);
 }
 
@@ -95,8 +94,8 @@ function onInit() {
   });
 
   NRF.setServices({
-    LIGHT_SERVICE_ID : {
-      LIGHT1_CHARACTERISTIC_ID : {
+    0xBCDE: {
+      0xABCD: {
         value : state[CHANNEL1], // optional
         maxLen : 1, // optional (otherwise is length of initial value)
         broadcast : false, // optional, default is false
@@ -123,7 +122,7 @@ function onInit() {
           console.log("Got ", evt.data); // an ArrayBuffer
         }
       },
-      LIGHT2_CHARACTERISTIC_ID : {
+      0xABCE: {
         value : state[CHANNEL2], // optional
         maxLen : 1, // optional (otherwise is length of initial value)
         broadcast : false, // optional, default is false
@@ -158,4 +157,9 @@ function onInit() {
   setWatch(function(e) {
     lightToggle(CHANNEL1);
   }, TS1, { repeat:true, edge:"falling", debounce: 50 });
+
+  setWatch(function(e) {
+    lightToggle(CHANNEL2);
+  }, TS2, { repeat:true, edge:"falling", debounce: 50 });
 }
+
