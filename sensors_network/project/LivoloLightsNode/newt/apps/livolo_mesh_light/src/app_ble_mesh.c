9ed9df2c79cd6de18d651cb402eee001739d2e1f
Marius Tiutiu <mtiutiu@softvision.ro>
2019-10-28 16:31:05 +0200

    ref

--- sensors_network/project/LivoloLightsNode/newt/apps/livolo_mesh_light/src/app_ble_mesh.c
+++ sensors_network/project/LivoloLightsNode/newt/apps/livolo_mesh_light/src/app_ble_mesh.c
@@ -33,64 +33,65 @@ static int output_number(bt_mesh_output_action_t action, uint32_t number);
 #endif
 
 static struct bt_mesh_cfg_srv cfg_srv = {
-    .relay = BT_MESH_RELAY_DISABLED,
-    .beacon = BT_MESH_BEACON_ENABLED,
+  .relay = BT_MESH_RELAY_DISABLED,
+  .beacon = BT_MESH_BEACON_ENABLED,
 #if MYNEWT_VAL(BLE_MESH_FRIEND)
-    .frnd = BT_MESH_FRIEND_ENABLED,
+  .frnd = BT_MESH_FRIEND_ENABLED,
 #else
-    .gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
+  .gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
 #endif
 #if MYNEWT_VAL(BLE_MESH_GATT_PROXY)
-    .gatt_proxy = BT_MESH_GATT_PROXY_ENABLED,
+  .gatt_proxy = BT_MESH_GATT_PROXY_ENABLED,
 #else
-    .gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
+  .gatt_proxy = BT_MESH_GATT_PROXY_NOT_SUPPORTED,
 #endif
-    .default_ttl = 7,
+  .default_ttl = 7,
 
-    /* 3 transmissions with 20ms interval */
-    .net_transmit = BT_MESH_TRANSMIT(2, 20),
-    .relay_retransmit = BT_MESH_TRANSMIT(2, 20),
+  /* 3 transmissions with 20ms interval */
+  .net_transmit = BT_MESH_TRANSMIT(2, 20),
+  .relay_retransmit = BT_MESH_TRANSMIT(2, 20),
 };
 
 static struct bt_mesh_health_srv health_srv = {
-    .cb = &health_srv_cb,
+  .cb = &health_srv_cb,
 };
 
 static const struct bt_mesh_model_op gen_onoff_op[] = {
-    {BT_MESH_MODEL_OP_2(0x82, 0x01), 0, gen_onoff_get},
-    {BT_MESH_MODEL_OP_2(0x82, 0x02), 2, gen_onoff_set},
-    {BT_MESH_MODEL_OP_2(0x82, 0x03), 2, gen_onoff_set_unack},
-    BT_MESH_MODEL_OP_END,
+  {BT_MESH_MODEL_OP_2(0x82, 0x01), 0, gen_onoff_get},
+  {BT_MESH_MODEL_OP_2(0x82, 0x02), 2, gen_onoff_set},
+  {BT_MESH_MODEL_OP_2(0x82, 0x03), 2, gen_onoff_set_unack},
+  BT_MESH_MODEL_OP_END,
 };
 
 static struct bt_mesh_model root_models[] = {
-    BT_MESH_MODEL_CFG_SRV(&cfg_srv),
-    BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub),
-    BT_MESH_MODEL(BT_MESH_MODEL_ID_GEN_ONOFF_SRV, gen_onoff_op, &gen_onoff_pub, NULL)};
+  BT_MESH_MODEL_CFG_SRV(&cfg_srv),
+  BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub),
+  BT_MESH_MODEL(BT_MESH_MODEL_ID_GEN_ONOFF_SRV, gen_onoff_op, &gen_onoff_pub, NULL)
+};
 
 static struct bt_mesh_elem elements[] = {
-    BT_MESH_ELEM(0, root_models, BT_MESH_MODEL_NONE),
+  BT_MESH_ELEM(0, root_models, BT_MESH_MODEL_NONE),
 };
 
 static const struct bt_mesh_comp comp = {
-    .cid = CID_NUMBER,
-    .elem = elements,
-    .elem_count = ARRAY_SIZE(elements),
+  .cid = CID_NUMBER,
+  .elem = elements,
+  .elem_count = ARRAY_SIZE(elements),
 };
 
 static const struct bt_mesh_prov prov = {
-    .uuid = MYNEWT_VAL(BLE_MESH_DEV_UUID),
+  .uuid = MYNEWT_VAL(BLE_MESH_DEV_UUID),
 #if (MYNEWT_VAL(BLE_MESH_OOB_PROV_ENABLED))
-    .output_size = 4,
-    .output_actions = BT_MESH_DISPLAY_NUMBER,
-    .output_number = output_number,
+  .output_size = 4,
+  .output_actions = BT_MESH_DISPLAY_NUMBER,
+  .output_number = output_number,
 #else
-    .output_size = 0,
-    .output_actions = 0,
-    .output_number = 0,
+  .output_size = 0,
+  .output_actions = 0,
+  .output_number = 0,
 #endif
-    .complete = prov_complete,
-    .reset = prov_reset
+  .complete = prov_complete,
+  .reset = prov_reset
 };
 
 static void gen_onoff_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx) {
