39520ccc9327ea035d68fe08c40efbc481bcd256
mtiutiu <marius.tiutiu@softvision.com>
2020-12-28 18:39:44 +0200

    ref

--- sensors_network/project/SerialGateway/src/main.cpp
+++ sensors_network/project/SerialGateway/src/main.cpp
@@ -1,77 +1,46 @@
 /**
-* The MySensors Arduino library handles the wireless radio link and protocol
-* between your home built sensors/actuators and HA controller of choice.
-* The sensors forms a self healing radio network with optional repeaters. Each
-* repeater and gateway builds a routing tables in EEPROM which keeps track of the
-* network topology allowing messages to be routed to nodes.
-*
-* Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
-* Copyright (C) 2013-2015 Sensnology AB
-* Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
-*
-* Documentation: http://www.mysensors.org
-* Support Forum: http://forum.mysensors.org
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* version 2 as published by the Free Software Foundation.
-*
-*******************************
-*
-* DESCRIPTION
-* The ArduinoGateway prints data received from sensors on the serial link.
-* The gateway accepts input on seral which will be sent out on radio network.
-*
-* The GW code is designed for Arduino Nano 328p / 16MHz
-*
-* Wire connections (OPTIONAL):
-* - Inclusion button should be connected between digital pin 3 and GND
-* - RX/TX/ERR leds need to be connected between +5V (anode) and digital pin 6/5/4 with resistor 270-330R in a series
-*
-* LEDs (OPTIONAL):
-* - To use the feature, uncomment any of the MY_DEFAULT_xx_LED_PINs
-* - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
-* - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
-* - ERR (red) - fast blink on error during transmission error or recieve crc error
-*
-*/
-
-// Enable debug prints to serial monitor
-//#define MY_DEBUG
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of
+ *the network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2019 Sensnology AB
+ * Full contributor list:
+ *https://github.com/mysensors/MySensors/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************/
 
 
-// Enable and select radio type attached
-//#define MY_RADIO_NRF24
-//#define MY_RFM69_NEW_DRIVER
 #define MY_RADIO_RFM69
-#define MY_RFM69_FREQUENCY RFM69_868MHZ
+//#define MY_IS_RFM69HW
+#define MY_RFM69_FREQUENCY (RFM69_868MHZ)
+#define MY_RFM69_NEW_DRIVER
 
-// Set LOW transmit power level as default, if you have an amplified NRF-module and
-// power your radio separately with a good regulator you can turn up PA level.
-//#define MY_RF24_PA_LEVEL RF24_PA_LOW
+#ifdef MY_IS_RFM69HW
+#define MY_RFM69_TX_POWER_DBM (20)
+#else
+#define MY_RFM69_TX_POWER_DBM (13)
+#endif
 
 // Enable serial gateway
 #define MY_GATEWAY_SERIAL
 
-#define MY_TRANSPORT_DISCOVERY_INTERVAL_MS 86400000UL // 24h network discovery interval
-
-// Define a lower baud rate for Arduino's running on 8 MHz (Arduino Pro Mini 3.3V & SenseBender)
+// Define a lower baud rate for Arduinos running on 8 MHz (Arduino Pro Mini 3.3V
+// & SenseBender)
 #if F_CPU == 8000000L
 #define MY_BAUD_RATE 38400
+#else
+#define MY_BAUD_RATE 115200
 #endif
-
-// Enable inclusion mode
-//#define MY_INCLUSION_MODE_FEATURE
-// Enable Inclusion mode button on gateway
-//#define MY_INCLUSION_BUTTON_FEATURE
-
-// Inverses behavior of inclusion button (if using external pullup)
-//#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
-
-// Set inclusion mode duration (in seconds)
-//#define MY_INCLUSION_MODE_DURATION 60
-// Digital pin used for inclusion mode button
-//#define MY_INCLUSION_MODE_BUTTON_PIN  3
 
 // Set blinking period
 #define MY_DEFAULT_LED_BLINK_PERIOD 300
@@ -81,29 +50,20 @@
 
 // Flash leds on rx/tx/err
 // Uncomment to override default HW configurations
-#define MY_DEFAULT_ERR_LED_PIN A2  // Error led pin
-#define MY_DEFAULT_RX_LED_PIN  A0  // Receive led pin
-#define MY_DEFAULT_TX_LED_PIN  A1  // the PCB, on board LED
+#define MY_DEFAULT_ERR_LED_PIN 4 // Error led pin
+#define MY_DEFAULT_RX_LED_PIN 5  // Receive led pin
+#define MY_DEFAULT_TX_LED_PIN 6  // the PCB, on board LED
 
-#include <Arduino.h>
-#include <SPI.h>
 #include <MySensors.h>
-#include <avr/wdt.h>
-
-void before() {
-  wdt_disable();
-  wdt_enable(WDTO_8S);
-}
 
 void setup() {
-// Setup locally attached sensors
+  // Setup locally attached sensors
 }
 
 void presentation() {
-// Present locally attached sensors
+  // Present locally attached sensors
 }
 
 void loop() {
-  wdt_reset();
-// Send locally attached sensor data here
+  // Send locally attached sensor data here
 }
